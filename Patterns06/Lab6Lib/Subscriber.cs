0400083af4509691e827cdeac36e3457d4539589
Romaska-uwu <149205053+Romaska-uwu@users.noreply.github.com>
2025-10-02 09:11:19 +0300

    Add files via upload

---
+++ Patterns06/Lab6Lib/Subscriber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Lib
+{
+    public class SubscriberA : ISubscriber
+    {
+        public void update(string eventname)
+        {
+            Console.WriteLine(string.Format("Subscriber: {0}, Event: {1}", 'A', eventname));
+        }
+    }
+    public class SubscriberB : ISubscriber
+    {
+        public void update(string eventname)
+        {
+            Console.WriteLine(string.Format("Subscriber: {0}, Event: {1}", 'B', eventname));
+        }
+    }
+    public class SubscriberC : ISubscriber
+    {
+        public void update(string eventname)
+        {
+            Console.WriteLine(string.Format("Subscriber: {0}, Event: {1}", 'C', eventname));
+        }
+    }
+}
